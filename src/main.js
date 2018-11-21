9e5cdd3501af22a460df703384bd0d39acdf7026
Wonho Kim <rhwhdeodhkd@gmail.com>
2018-11-21 21:05:18 +0900

    setting init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import './plugins/axios';
+import './styles.scss';
+import App from './App.vue';
+import router from './router';
+import ElementUI from 'element-ui';
+import store from './store';
+import locale from 'element-ui/lib/locale/lang/ko';
 
-Vue.config.productionTip = false
+Vue.use(ElementUI, { locale });
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {},
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage,
+      key: 'your-key',
+      // paths: ["your-path"]
+    }),
+  ],
+});
